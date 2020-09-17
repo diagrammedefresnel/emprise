17ab01117c15e31fa07b499d53f46b79753a3b62
férié <diagrammedefresnel@gmail.com>
2020-09-17 02:33:50 +0000

    add admin files

---
+++ admin/articles/edit.php
@@ -0,0 +1,90 @@
+<?php include('../inc/header.php') ?>
+
+          <!-- Begin Page Content -->
+          <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="d-sm-flex align-items-center justify-content-between
+              mb-4">
+              <h1 class="h1 mb-5 text-gray-800 mt-4">Création d'un Article</h1>
+            </div>
+
+            <!-- Content Row -->
+
+              <!-- Dropdown Card Example -->
+              <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center
+                  justify-content-between col-md-12 col-xl-12">
+                  <h6 class="m-0 font-weight-bold text-warning">Veuillez à remplir tous les champs</h6>
+                  <div class="dropdown no-arrow">
+                    <a class="dropdown-toggle" href="#" role="button"
+                      id="dropdownMenuLink" data-toggle="dropdown"
+                      aria-haspopup="true" aria-expanded="false">
+                      <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right shadow
+                      animated--fade-in" aria-labelledby="dropdownMenuLink">
+                      <div class="dropdown-header">Action:</div>
+                      <a class="dropdown-item" href="#">Lister les articles</a>
+                      <a class="dropdown-item" href="home.html">Annuler la saisie</a>
+                      <div class="dropdown-divider"></div>
+                    </div>
+                  </div>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+
+
+                  <form>
+
+                    <div class="custom-file mb-4">
+                      <input type="file" class="custom-file-input" required>
+                      <label class="custom-file-label" for="validatedCustomFile">Choississez votre image...</label>
+                      <div class="invalid-feedback">Example invalid custom file feedback</div>
+                    </div>
+
+                    <div class="form-group">
+                      <label for="title">Titre de L'article</label> 
+                      <input id="title" name="title" placeholder="Titre de l'article" type="text" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                      <label for="legende">Légende</label> 
+                      <input id="legende" name="legende" placeholder="Légende" type="text" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                      <label for="Chapeau">Chapeau</label> 
+                      <input id="Chapeau" name="Chapeau" placeholder="chapeau" type="text" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                      <label for="content">Corps du texte</label> 
+                      <textarea id="content" name="content" cols="40" rows="10" class="form-control"></textarea>
+                    </div> 
+
+                    <div class="form-group">
+                      <button name="submit" type="submit" class="btn btn-primary">Poster l'article</button>
+                    </div>
+
+                  </form>
+                  
+
+
+
+
+
+                </div>
+              </div>
+
+              </div>
+
+
+            </div>
+
+
+          </div>
+          <!-- End of Content Wrapper -->
+
+  <?php include('../inc/footer.php') ?>
---
+++ admin/home.php
@@ -0,0 +1,63 @@
+<?php include('inc/header.php') ?>
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4 ">
+            <h1 class="h1 mb-5 text-gray-800 mt-4">Bienvenue Ben</h1>
+          </div>
+
+          <!-- Content Row -->
+          <div class="row">
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-6 col-md-6 mb-4">
+              <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-3"><h2>Articles</h2></div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800 mb-5">Nombre d'articles du blog : 52</div>
+                      <div class="mt-3 text-center">
+                        <a href="articles/edit.php" class="btn btn-primary">Ecrire un article</a>
+                        <a href="" class="btn btn-success">Lister les articles</a>
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+            <!-- Pending Requests Card Example -->
+            <div class="col-xl-6 col-md-6 mb-4">
+              <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-warning text-uppercase mb-3 "><h2>Message</h2></div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800 mb-5">Nombres de messages : 18</div>
+                      <div class="mt-3 text-center">
+                        <a href="" class="btn btn-primary">Ecrire un message</a>
+                        <a href="" class="btn btn-success">Lister les messages</a>
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-comments fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+
+    <?php include('inc/footer.php') ?>
